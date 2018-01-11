0f1bf0e2dadc4c8ef25c3eccc47b0c7085ae0cd2
Marnix van de Sande <m.a.j.vandesande@students.uu.nl>
2018-01-11 17:21:27 +0100

    Data analysis script roughly done

--- data_analysis.r
+++ data_analysis.r
@@ -17,11 +17,23 @@ conflicts$date_end <- as.Date(conflicts$date_end, '%Y-%m-%d') #Times 2
 conflicts.one <- conflicts %>% filter(year < 2010)
 conflicts.two <- conflicts %>% filter(year > 2010)
 
-ggplot(data = conflicts.one) +
-  geom_polygon(data = map_data('kenya'), mapping = aes(x = long, y = lat, group=group), fill = NA, colour='black')
+#Create a map of Kenya
+map_kenya <- map_data('world') %>% filter(region == 'Kenya')
 
+#Plot conflicts during the aftermath of the 2007 elections, size = number of deaths, color = type of violence
+ggplot(data = conflicts.one) +
+  geom_polygon(data = map_kenya, mapping = aes(x = long, y = lat, group=group), fill = NA, colour='black') +
+  geom_point(mapping = aes(x=longitude, y=latitude, color=type_of_violence, size=best))
+
+#Plot conflicts during the aftermath of the 2013 elections, size = number of deaths, color = type or violence
+ggplot(data = conflicts.two) +
+  geom_polygon(data = map_kenya, mapping = aes(x = long, y = lat, group=group), fill = NA, colour='black') +
+  geom_point(mapping = aes(x=longitude, y=latitude, color=type_of_violence, size=best))
+
+#Plot graph showing the number of deaths during the year after the 2007 elections
 ggplot(data = conflicts.one) +
   geom_point(mapping = aes(x=date_start, y=best))
 
+#Plot graph showing the number of deaths during the year after the 2013 elections
 ggplot(data = conflicts.two) +
   geom_point(mapping = aes(x=date_start, y=best))
